4bc63a6baff868635225cdaf46e7fe9ad6cd6b1c
Thiago Mattos <thiagobuenosairesmattos@gmail.com>
2021-02-09 21:05:31 -0300

    Initial commit

---
+++ frases.js
@@ -0,0 +1,36 @@
+const frases = [
+    [
+        "De acordo com Schrödinger, ",
+        "Uma recente pesquisa demonstra que ",
+        "Acadêmicos debatem sobre ",
+        "Experimentos demonstram a probabilidade de que ",
+        "Fundamentalmente, ",
+        "Evidentemente, ",
+        "Por dedução direta, ",
+        "Obviamente ",
+        "Utilizamos o método ciêntifico para demonstrar que "
+    ],
+    [
+        "a crescente busca pela teoria unificada ",
+        "a energia cinética do objeto ",
+        "a derivada da função ",
+        "o experimento realizado "
+    ],
+    [
+        "nos obriga à análise ",
+        "exige a precisão e a definição ",
+        "assume a continuidade ",
+        "é uma das consequências ",
+        "não pode mais se dissociar ",
+        "estimula a diferenciação ",
+        "aponta para a convergência ",
+        "afeta positivamente a correta previsão "
+    ],
+    [
+        "das novas proposições.",
+        "do levantamento de dados experimentais.",
+        "dos métodos utilizados na avaliação de resultados.",
+        "das conjecturas inferidas.",
+        "da geometria euclidiana."
+    ]
+  ];
